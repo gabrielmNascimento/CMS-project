1b86c4b0587b4c4c01c6278987d9241d4ecef7f5
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2023-11-12 16:59:36 -0300

    Create Bottom.vue

---
+++ Portfolio/resources/js/components/layout/Bottom.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-inline">ve. 2021</div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Bottom'
+    }
+</script>
