acd68351b73a5030eb207945a0c92f6ddbf540b0
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2023-12-20 20:39:26 -0300

    Create reparos.js

---
+++ Portfolio/resources/js/types/reparos.js
@@ -0,0 +1,8 @@
+const Reparos = [
+    {id: 'Refazer', option: 'Refazer'}, 
+    {id: 'Censo', option: 'Primeiro Clarinete'},
+    {id: 'Erro', option: 'Corrigir Erros'},
+    {id: 'Para Inspetor', option: 'Para Inspetor'}  
+]
+
+export default Reparos
