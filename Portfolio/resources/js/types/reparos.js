9fc6ccec85a453274d9d63c8d3f7d051d87487c5
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2023-12-21 19:43:23 -0300

    Update reparos.js

--- Portfolio/resources/js/types/reparos.js
+++ Portfolio/resources/js/types/reparos.js
@@ -1,6 +1,6 @@
 const Reparos = [
     {id: 'Refazer', option: 'Refazer'}, 
-    {id: 'Censo', option: 'Primeiro Clarinete'},
+    {id: 'Clarinete', option: 'Primeiro Clarinete'},
     {id: 'Erro', option: 'Corrigir Erros'},
     {id: 'Para Inspetor', option: 'Para Inspetor'}  
 ]
