f6b7ed3dcc645e7575214d113c46c2701b02625b
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2023-12-25 19:46:04 -0300

    Create Page.vue

---
+++ Portfolio/resources/js/components/layout/Page.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-12 aqui">
+                        <slot name="alert"></slot>
+                    </div>
+                    <div class="col-sm-12">
+                        <h1 class="m-0 text-dark">{{ title }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Page',
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
