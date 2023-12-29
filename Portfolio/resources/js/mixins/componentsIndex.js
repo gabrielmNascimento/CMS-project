6d31ce4f73f4f007b8eb4922f83cb60de2c96cdc
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2023-12-29 16:59:16 -0300

    Create componentsIndex.js

---
+++ Portfolio/resources/js/mixins/componentsIndex.js
@@ -0,0 +1,12 @@
+import Page from '../components/layout/Page'
+import Card from '../components/content/Card'
+import Alert from '../components/content/Alert'
+import DataGrid from '../components/content/DataGrid'
+import AppFilter from '../components/content/AppFilter'
+import InputText from '../components/form/InputText'
+import ModalDelete from '../components/content/ModalDelete'
+
+export default {
+    components: { Card, Page, Alert, DataGrid, AppFilter, InputText, ModalDelete }
+}
+
