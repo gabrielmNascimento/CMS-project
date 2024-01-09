ecb2f96ad32ed2f2599f902e5567d55876aaea9e
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-01-09 20:59:31 -0300

    Create InputText.vue

---
+++ Portfolio/resources/js/components/form/InputText.vue
@@ -0,0 +1,33 @@
+<template>
+    <input type="text" class="form-control" :readonly="disable" autocomplete="off" v-model="value" :class="[error ? 'is-invalid' : '']">
+</template>
+
+<script>
+    import { computed } from 'vue'
+
+    export default  {
+        name: 'InputText',
+        props: {
+            error: {
+                type: Array
+            },
+            modelValue: {
+                type: String,
+                default: ''
+            },
+            disable: {
+                type: Boolean,
+                default: false
+            }
+        },
+        setup(props, {emit}) {
+            const value = computed({
+                get: () => props.modelValue,
+                set: value => {
+                    emit('update:modelValue', value)
+                }
+            })
+            return { value }
+        }
+    }
+</script>
