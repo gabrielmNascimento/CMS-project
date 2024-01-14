47c0c3c0de220a98c419d3ee3e3685f964855c7a
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-01-14 15:14:50 -0300

    Create componentsForm.js

---
+++ Portfolio/resources/js/mixins/componentsForm.js
@@ -0,0 +1,12 @@
+import Page from '../components/layout/Page'
+import Card from '../components/content/Card'
+import Alert from '../components/content/Alert'
+import FormError from '../components/content/FormError'
+import InputLabel from '../components/form/InputLabel'
+import InputText from '../components/form/InputText'
+import SubmitButton from '../components/form/SubmitButton'
+import InputFile from '../components/form/InputFile'
+
+export default {
+    components: { Card, Page, FormError, InputLabel, InputText, SubmitButton, Alert, InputFile }
+}
