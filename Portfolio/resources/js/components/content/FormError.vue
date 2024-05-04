4d9a0f9976236820717b9fc1452e75b892bf85b6
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-05-04 07:24:44 -0300

    Create FormError.vue

---
+++ Portfolio/resources/js/components/content/FormError.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="form-group">
+        <slot></slot>
+        <small class="text-danger" v-if="error">{{ error[0] }}</small>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FormError',
+        props: {
+            error: {
+                type: Array
+            }
+        }
+    }
+</script>
