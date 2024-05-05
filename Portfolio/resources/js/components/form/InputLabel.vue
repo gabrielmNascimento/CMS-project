9b201b7d0f6e9dd89fb461bbf9a29eb4304c2569
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-05-05 02:20:57 -0300

    Create InputLabel.vue

---
+++ Portfolio/resources/js/components/form/InputLabel.vue
@@ -0,0 +1,26 @@
+<template>
+    <label class="col-form-label">
+        <span class="text-danger text-bold" v-if="needed">*</span> {{ empty ? '&nbsp;' : label }}{{ empty ? '' : ':' }}</label>
+</template>
+
+<script>
+    export default  {
+        name: 'InputLabel',
+        props: {
+            label: {
+                type: String,
+                required: true
+            },
+            needed: {
+                type: Boolean,
+                required: false,
+                default: false
+            },
+            empty: {
+                type: Boolean,
+                required : false,
+                default: false
+            }
+        }
+    }
+</script>
