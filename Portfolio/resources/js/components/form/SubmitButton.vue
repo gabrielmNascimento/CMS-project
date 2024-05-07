f9aa0e51e59ddb664d1d6c15d6b6ae3d2149529c
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-05-07 07:42:19 -0300

    Create SubmitButton.vue

---
+++ Portfolio/resources/js/components/form/SubmitButton.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="text-center">
+        <button v-if="submitted" type="button" class="btn btn-warning btn-sm font-weight-bold" disabled>
+            Enviando <i class="fa fa-sync fa-spin ml-2"></i>
+        </button>
+        <button v-if="valid && !submitted" type="button" class="btn btn-sm" :class="classe" @click.prevent="submit">
+            {{ label }}</button>
+        <button v-if="clean" type="button" class="btn btn-default btn-sm ml-2" @click.prevent="cleanData">Limpar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SubmitButton',
+        props: {
+            classe: {
+                required: false,
+                type: String,
+                default: 'btn-success'
+            },
+            label: {
+                required: false,
+                type: String,
+                default: 'Salvar'
+            },
+            valid: {
+                required: true
+            },
+            submitted: {
+                required: true,
+                type: Boolean
+            },
+            clean: {
+                required: false,
+                type: Boolean,
+                default: false
+            }
+        },
+        setup(props, {emit}) {
+            const submit = () => {
+                emit('send')
+            }
+            const cleanData = () => {
+                emit('cleanData')
+            }
+            return { submit, cleanData }
+        }
+    }
+</script>
