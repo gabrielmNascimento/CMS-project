e8a89324f7e6ae7d257de576a6532a96e3263a23
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-05-31 03:33:34 -0300

    Create LayerSwitch.vue

---
+++ Portfolio/resources/js/components/geo/LayerSwitch.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p class="text-uppercase font-italic mb-2">Camadas base</p>
+        <div class="col-md-12" v-for="(item, i) in items" :key="i">
+            <div class="custom-control custom-radio">
+                <input class="custom-control-input" type="radio" :id="`baseLayer_${i}`"
+                    :value="i" v-model="selected" @change="changeLayer">
+                <label :for="`baseLayer_${i}`" class="custom-control-label">{{ item }}</label>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { useOlLayerSwitcher } from '../../composition/geo/olLayerSwitcher'
+
+    export default {
+        name: 'LayerSwitcher',
+        setup() {
+            return { ...useOlLayerSwitcher() }
+        }
+    }
+</script>
