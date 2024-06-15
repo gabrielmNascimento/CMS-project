9ddef2379136cff5adf7aacc32c94d0b76b0e715
gabrielmNascimento <82829098+gabrielmNascimento@users.noreply.github.com>
2024-06-15 02:57:10 -0300

    Create Geo.vue

---
+++ Portfolio/resources/js/components/layout/navbar/Geo.vue
@@ -0,0 +1,19 @@
+<template>
+    <li class="nav-item">
+        <router-link v-if="permission('gis.telesena') || permission('gis.doom')" class="nav-link" to="/mapa">
+            <i class="fas fa-globe-americas"></i>
+        </router-link>
+    </li>
+</template>
+
+<script>
+    import { useAuth } from '../../../composition/auth'
+
+    export default {
+        name: 'Geo',
+        setup() {
+            const { permission, dataUser } = useAuth()
+            return { permission, dataUser }
+        }
+    }
+</script>
